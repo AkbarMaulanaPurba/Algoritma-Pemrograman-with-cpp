ec5969e818c2679d3f8fc66b68878fdf67bb4991
Akbar Maulana Purba <akbarpurba499@gmail.com>
2024-11-08 21:58:55 +0700

    Add files via upload

---
+++ stringFunction.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+	string str1 = "STMIK";
+	string str2 = "PENUSA";
+	
+	//concatenation = mengabung lebih dari satu string
+	string result = str1 + " " + str2;
+	cout << "Concatenated: " << result << endl;
+	//length panjang string
+	 size_t length = result.length();
+	 cout << "Length: " << length << endl;
+	//substring mengabil string
+	 string sub = result.substr(6,6);
+	 cout << "Substring: " << sub << endl;
+	 //find mencari posisi
+	 size_t pos =  result.find("PENUSA");
+	 cout << "Position of 'PENUSA': " << pos << endl;
+	 // replace menyisipkan atau menambahkan
+	 result.replace(0, 5, "Hi");
+	 cout << "Replaced: " << result << endl;
+	return 0;
+}
